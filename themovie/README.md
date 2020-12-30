3564a18ea3513931dc8ac2eded1f3aec88e18412
kalcoder <jesusreeb@hotmail.com>
2020-12-30 03:05:44 -0300

    firs commit

--- themovie/README.md
+++ themovie/README.md
@@ -1,4 +1,4 @@
-# Movie
+# Themovie
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.0.3.
 
--- themovie/src/app/app.component.spec.ts
+++ themovie/src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'movie'`, () => {
+  it(`should have as title 'themovie'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('movie');
+    expect(app.title).toEqual('themovie');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('movie app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('themovie app is running!');
   });
 });
